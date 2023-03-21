7770b7bf6500df61027421f9ae9f605f7c6ebfbf
Master811129 <37829730+Master811129@users.noreply.github.com>
2023-03-21 14:40:39 +0330

    Disabling Wayland properly

--- src/main.cpp
+++ src/main.cpp
@@ -10,18 +10,10 @@ int main (int argc, char **argv)
     //Gempyre::set_debug();
     mywindow w(Resourcesh,"index.html","Ahang",1152,648);
     //Qt6 is a bit quirky when it comes to wayland so i disabled it
-    const char *waylandenv = std::getenv("WAYLAND_DISPLAY");
-    setenv("WAYLAND_DISPLAY", "", true);
-    std::thread t([waylandenv]{
-        using namespace std::literals::chrono_literals;
-        std::this_thread::sleep_for(1s);
-        if(waylandenv!=nullptr)setenv("WAYLAND_DISPLAY", waylandenv, true);
-    });
+    setenv("QT_QPA_PLATFORM", "xcb", true);
     //TODO maybe an icon manager?
-
     w.run();
     std::cout << "Back To main():\nCleaning up\n";
-    t.join();
     std::cout << "Program has ended successfully✅" <<std::endl;
     return 0;
 }
