0f7ececf71ebc108541836a038979647dee5eac3
Master811129 <37829730+Master811129@users.noreply.github.com>
2023-03-31 23:25:36 +0330

    cross platform solution:setting environment vars🐧🪟🍎

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main (int argc, char **argv)
     //Gempyre::set_debug();
     mywindow w(Resourcesh,"index.html","Ahang",1152,648);
     //Qt6 is a bit quirky when it comes to wayland so i disabled it
-    setenv("QT_QPA_PLATFORM", "xcb", true);
+    putenv((char *)("QT_QPA_PLATFORM=xcb"));
     //TODO maybe an icon manager?
     w.run();
     std::cout << "Back To main():\nCleaning up\n";
