5c7924fb97040d6f01c3ae2dc67848a6a4aec8fb
Master811129 <37829730+Master811129@users.noreply.github.com>
2023-04-29 00:58:08 +0330

    safer C++ casting

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main (int argc, char **argv)
     //Gempyre::set_debug();
     mywindow w(Resourcesh,"index.html","Ahang",1152,648);
     //Qt6 is a bit quirky when it comes to wayland so i disabled it
-    putenv((char *)("QT_QPA_PLATFORM=xcb"));
+    putenv(const_cast<char *>("QT_QPA_PLATFORM=xcb"));
     //TODO maybe an icon manager?
     w.run();
     std::cout << "Back To main():\nCleaning up\n";
